44ed87b370f98bc24d68da16ab8505df683afe66
Ted Philip Lat <lattedphilip15@gmail.com>
2016-11-11 16:40:53 +0800

    change balloon to a ball

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ platform.x = display.contentCenterX
 platform.y = display.contentHeight - 25
 
 --load balloon
-local balloon = display.newImageRect("balloon.png", 112, 112)
+local balloon = display.newImageRect("basketball.png", 112, 112)
 balloon.x = display.contentCenterX
 balloon.y = display.contentCenterY
 balloon.alpha = 0.8
@@ -26,7 +26,7 @@ physics.setGravity(0, 9.8)
 
 --add phydics to objects
 physics.addBody(platform, "static")
-physics.addBody(balloon, "dynamic", { radius = 49, bounce = 0.3})
+physics.addBody(balloon, "dynamic", { radius = 53, bounce = 0.3})
 
 local function pushBalloon()
 	balloon:applyLinearImpulse(0, -0.5, balloon.x, balloon.y)
